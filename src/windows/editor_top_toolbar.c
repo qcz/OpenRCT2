b046543c8d01bf49ad0a58dab48edeef3e69e548
qcz <bdanee88@gmail.com>
2014-10-26 12:28:00 +0100

    Fix encoding

--- src/windows/editor_top_toolbar.c
+++ src/windows/editor_top_toolbar.c
@@ -1,5 +1,5 @@
-/*****************************************************************************
-* Copyright (c) 2014 D�niel Tar, Ted John
+﻿/*****************************************************************************
+* Copyright (c) 2014 Dániel Tar, Ted John
 * OpenRCT2, an open source clone of Roller Coaster Tycoon 2.
 *
 * This file is part of OpenRCT2.
